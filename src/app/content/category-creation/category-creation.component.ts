7345ed2e93374c259f0f5bec5dabdada1499d2fd
lseremak <llseremak@gmail.com>
2022-10-30 20:52:09 +0100

    create category.service.ts

--- src/app/content/category-creation/category-creation.component.ts
+++ src/app/content/category-creation/category-creation.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Category } from "../../../dto/category";
+import { CategoryService } from "../../../service/category.service";
 
 @Component({
   selector: 'app-category-creation',
   templateUrl: './category-creation.component.html',
   styleUrls: ['./category-creation.component.scss']
 })
-export class CategoryCreationComponent implements OnInit {
+export class CategoryCreationComponent {
 
-  constructor() { }
+  public categories$;
 
-  ngOnInit(): void {
+  category: Category = {
+    name: null
+  };
+
+  constructor(private categoryService: CategoryService,
+              private modalService: NgbModal) {
+    this.categories$ = this.categoryService.getCategories();
   }
 
+  open(content) {
+    this.resetFormFields()
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then(
+      () => {
+        console.log(this.category)
+        this.categoryService.createCategory(this.category)
+          .subscribe(console.log);
+      },
+      () => {
+        console.log("Bill creation canceled")
+      }
+    );
+  }
+
+  resetFormFields() {
+    this.category.name = null;
+  }
 }
