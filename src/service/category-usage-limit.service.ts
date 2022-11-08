b786802efbbcaeafdb2a59825d31d1653d932aff
lseremak <llseremak@gmail.com>
2022-11-08 17:06:49 +0100

    implement category-usage-limit.service.ts

---
+++ src/service/category-usage-limit.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+import { BehaviorSubject, catchError, debounceTime, Observable, Subject, switchMap, tap } from "rxjs";
+import { CategoryUsageLimit } from "../dto/categoryUsageLimit";
+import { HttpUtils } from "../utils/http/httpClientUtils";
+
+export class CategoryUsageLimitService {
+
+  private static host = environment.billPlanHost
+  private static endpoint = "/category-usage-limit"
+
+  private _findCategoryUsageLimit$ = new Subject<void>();
+  private _categoryUsageLimit$ = new BehaviorSubject<CategoryUsageLimit>(null);
+  private _loading$ = new BehaviorSubject<boolean>(true);
+
+  constructor(private httpClient: HttpClient) {
+    this._findCategoryUsageLimit$
+      .pipe(
+        tap(() => this._loading$.next(true)),
+        debounceTime(200),
+        switchMap(() => this.findCategoryUsageLimits()),
+        tap(() => this._loading$.next(false))
+      )
+      .subscribe((result) => this._categoryUsageLimit$.next(result))
+  }
+
+  findCategoryUsageLimits(): Observable<CategoryUsageLimit[]> {
+    const url = HttpUtils.prepareUrl(CategoryUsageLimitService.host, CategoryUsageLimitService.endpoint);
+    return this.httpClient.get<CategoryUsageLimitService[]>(url, {headers: HttpUtils.prepareHeaders()})
+      .pipe(
+        catchError(HttpUtils.handleError),
+        tap(console.log)
+      );
+  }
+
+  refresh() {
+    this._findCategoryUsageLimit$.next();
+  }
+
+  get balance$() {
+    return this._categoryUsageLimit$.asObservable();
+  }
+
+  get loading$() {
+    return this._loading$.asObservable();
+  }
+}
