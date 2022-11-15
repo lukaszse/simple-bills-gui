0e78f9e47829fc189654e8570a6acddf1fdb2329
lseremak <llseremak@gmail.com>
2022-11-16 00:01:56 +0100

    Add deposit service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./content/home/home.component";
 import { TransactionsComponent } from "./content/transactions/transactions.component";
 import { CategoryComponent } from "./content/category/category.component";
+import { AssetsComponent } from "./content/assets/assets.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'transactions', component: TransactionsComponent},
-  {path: 'category', component: CategoryComponent}
+  {path: 'category', component: CategoryComponent},
+  {path: 'assets', component: AssetsComponent}
 ];
 
 @NgModule({
